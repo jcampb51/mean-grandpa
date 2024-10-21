136a3efee4198da9b0c8c5d57a9cc92e36a51dc1
James Campbell <jamesrcampbell.obb@gmail.com>
2024-10-21 10:50:38 -0500

    created fixtures and seeded database

--- grandpaproject/urls.py
+++ grandpaproject/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from grandpaapi.models import *
+from grandpaapi.views import *
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"users", Users, "user")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("api-token-auth", obtain_auth_token),
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
 ]
 
--- seed_database.sh
+++ seed_database.sh
@@ -5,6 +5,15 @@ rm -rf ./grandpaapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations grandpaapi
 python3 manage.py migrate grandpaapi
-python3 manage.py loaddata users
-python3 manage.py loaddata tokens
+# python3 manage.py loaddata users
+# python3 manage.py loaddata tokens
+python manage.py loaddata users.json
+python manage.py loaddata tokens.json
+python manage.py loaddata category.json
+python manage.py loaddata exercise.json
+python manage.py loaddata exercisecategory.json
+python manage.py loaddata kudo.json
+python manage.py loaddata workout.json
+python manage.py loaddata workoutcategory.json
+python manage.py loaddata workoutexercise.json
 
